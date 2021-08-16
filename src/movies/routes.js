dac16b272db64826129f862e86fffa99f879501a
Damián <dfinkelstein@wolox.com.ar>
2021-08-15 22:31:46 -0300

    implemented basic DB

--- src/movies/routes.js
+++ src/movies/routes.js
@@ -1,19 +1,14 @@
 const express = require('express');
+const { Movie } = require('../db/models/'); 
 
 const router = express.Router();
 
-const movies = [
-  {
-    title: '100 Dalmatas',
-    imageUrl: 'bla'
-  }
-];
-
 router
-  .get('/', (req, res) => {
-    res.send(movies)
+  .get('/', async (_, res) => {
+    const allMovies = await Movie.findAll();
+    res.send(allMovies);
   })
-  .post((req, res) => {
+  .post('/', async (req, res) => {
     if (!req.body.title || !req.body.imageUrl) {
       res.status(400).send({ error: 'Both `title` and `imageUrl` parameters should be present' });
     }
@@ -21,9 +16,8 @@ router
       res.status(400).send({ error: 'Title can only contain letters or numbers' });
     }
   
-    const newMovie = { title: req.body.title, imageUrl: req.body.imageUrl };
-    movies.push(newMovie)
-    res.send(newMovie);
+    const newMovie = await Movie.create({ title: req.body.title, imageUrl: req.body.imageUrl });
+    res.status(201).send(newMovie);
   });
 
 module.exports = { movieRouter: router }
