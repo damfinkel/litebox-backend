d66f38a90a89bdae1762f2b13e3bc9fb4868d5b0
Damián <dfinkelstein@wolox.com.ar>
2021-08-17 19:05:23 -0300

    added id in api response

--- src/movies/routes.js
+++ src/movies/routes.js
@@ -6,14 +6,17 @@ const router = express.Router();
 router
   .get('/', async (_, res) => {
     const allMovies = await Movie.findAll();
-    res.send(allMovies);
+    res.send(allMovies.map(({ id, title, imageUrl }) => ({ id, title, poster_path: imageUrl })));
   })
   .post('/', async (req, res) => {
     if (!req.body.title || !req.body.imageUrl) {
       res.status(400).send({ error: 'Both `title` and `imageUrl` parameters should be present' });
+      return;
     }
+
     if(!req.body.title.match(/[a-zA-Z0-9]/)) {
       res.status(400).send({ error: 'Title can only contain letters or numbers' });
+      return;
     }
   
     const newMovie = await Movie.create({ title: req.body.title, imageUrl: req.body.imageUrl });
